c83ed98291bd7e1bf16ca84bee4f1c0abe474142
rufus20145 <ivan20027749@gmail.com>
2024-03-18 00:17:27 +0300

    Добавлен пример inline обращения к боту в стартовом сообщении.

--- bot/src/main/java/ru/maipomogator/bot/processors/message/command/StartCommandProcessor.java
+++ bot/src/main/java/ru/maipomogator/bot/processors/message/command/StartCommandProcessor.java
@@ -6,7 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.request.ReplyKeyboardRemove;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.BaseResponse;
@@ -21,9 +22,17 @@ public class StartCommandProcessor extends AbstractCommandProcessor {
     @Override
     protected Collection<BaseRequest<?, ? extends BaseResponse>> process(Message msg, Long chatId) {
         SendMessage message = new SendMessage(chatId, "Приветствуем в нашем боте.\n"
-                + "Для выбора группы введите её номер или /newgroup для интерактивного выбора."
-                + " Для выбора преподавателя введите фамилию или ФИО целиком.")
-                        .replyMarkup(new ReplyKeyboardRemove());
+                + "Для выбора группы введите её номер или /newgroup для интерактивного выбора. "
+                + "Для выбора преподавателя введите фамилию или имя и отчество, или ФИО целиком.\n\n"
+                + "Также имеется поддержка inline-режима, благодаря которому можно отправить сообщение с расписанием в любом чате.")
+                        .replyMarkup(getKeyboard());
         return List.of(message);
+    }
+
+    private InlineKeyboardMarkup getKeyboard() {
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.addRow(new InlineKeyboardButton("Попробовать inline").switchInlineQueryCurrentChat(""));
+        keyboard.addRow(new InlineKeyboardButton("Отправить расписание в другой чат").switchInlineQuery(""));
+        return keyboard;
     }
 }
