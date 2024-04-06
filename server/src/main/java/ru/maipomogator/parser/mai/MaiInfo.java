80df07c1f6cb964f114eb2d9892cba059f658cfc
rufus20145 <ivan20027749@gmail.com>
2024-04-06 16:41:46 +0300

    Класс с информацией о факультетах и курсах МАИ преобразован в record

--- server/src/main/java/ru/maipomogator/parser/mai/MaiInfo.java
+++ server/src/main/java/ru/maipomogator/parser/mai/MaiInfo.java
@@ -2,12 +2,4 @@ package ru.maipomogator.parser.mai;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class MaiInfo {
-    private final List<String> faculties;
-    private final Integer numberOfCourses;
-}
+public record MaiInfo(List<String> faculties, Integer numberOfCourses) {}
