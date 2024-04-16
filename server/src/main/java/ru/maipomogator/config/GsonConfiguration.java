befcdacfa93ccabc5b7822842633df40fcc603fd
rufus20145 <ivan20027749@gmail.com>
2024-04-16 20:35:45 +0300

    Удалена случайная строка

--- server/src/main/java/ru/maipomogator/config/GsonConfiguration.java
+++ server/src/main/java/ru/maipomogator/config/GsonConfiguration.java
@@ -8,13 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import jakarta.persistence.SequenceGenerator;
 import lombok.extern.log4j.Log4j2;
 import ru.maipomogator.updaters.mai.adapters.GsonAdapter;
 
 @Log4j2
 @Configuration
-@SequenceGenerator(name = "123")
 public class GsonConfiguration {
     @Bean
     Gson gson(List<GsonAdapter> adapters) {
