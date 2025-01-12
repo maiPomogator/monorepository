e584b4dfc4cdbe6407e997f1e8a6b292ac75a11a
rufus20145 <ivan20027749@gmail.com>
2025-01-12 03:32:16 +0300

    Заменен TypeAdapter на JsonDeserializer для Group в процессе замены Jackson на Gson

--- server/src/main/java/ru/maipomogator/config/GsonConfiguration.java
+++ server/src/main/java/ru/maipomogator/config/GsonConfiguration.java
@@ -9,16 +9,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import lombok.extern.log4j.Log4j2;
-import ru.maipomogator.config.gson.adapters.GsonAdapter;
+import ru.maipomogator.config.gson.adapters.TypeableAdapter;
 
 @Log4j2
 @Configuration
 public class GsonConfiguration {
     @Bean
-    Gson gson(List<GsonAdapter> adapters) {
+    Gson gson(List<TypeableAdapter> adapters) {
         log.debug("Creating gson with {} adapters.", adapters.size());
         GsonBuilder builder = new GsonBuilder();
-        adapters.forEach(a -> builder.registerTypeAdapter(a.getType(), a));
+        adapters.forEach(a -> builder.registerTypeAdapter(a.getTargetType(), a));
         return builder.create();
     }
 }
