5b44a9d77f307f825ed650296465813b1019c866
rufus20145 <ivan20027749@gmail.com>
2025-01-20 23:40:36 +0300

    Добавлена передача в RestClient конвертера с gson

--- server/src/main/java/ru/maipomogator/config/RestClientConfiguration.java
+++ server/src/main/java/ru/maipomogator/config/RestClientConfiguration.java
@@ -1,16 +1,20 @@
 package ru.maipomogator.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.JettyClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfiguration {
     @Bean
-    RestClient restClient() {
+    RestClient restClient(List<HttpMessageConverter<?>> messageConverters) {
         return RestClient.builder().baseUrl("https://public.mai.ru/schedule/data/")
                 .requestFactory(new JettyClientHttpRequestFactory())
+                .messageConverters(messageConverters)
                 .build();
     }
 }
